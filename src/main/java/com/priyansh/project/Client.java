296179140f90deadfc8f6ab8790d59c364ba3dc8
Priyansh-Kumar-Pandey <133840423+Priyansh-Kumar-Pandey@users.noreply.github.com>
2023-09-12 09:36:43 +0530

    Update Client.java

--- src/main/java/com/priyansh/project/Client.java
+++ src/main/java/com/priyansh/project/Client.java
@@ -7,7 +7,7 @@ public class Client {
     public static void main(String[] args) {
         HTMLDocument document = HTMLDocument.current();
         HTMLElement div = document.createElement("div");
-        div.appendChild(document.createTextNode("TeaVM generated element"));
+        div.appendChild(document.createTextNode("my webhook is working perfectly"));
         document.getBody().appendChild(div);
     }
 }
